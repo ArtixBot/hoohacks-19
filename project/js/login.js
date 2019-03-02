59c38d1174a3c01bc6a3883285227267e83a6248
Christopher Raley <cr2fv@virginia.edu>
2019-03-02 14:26:09 -0500

    Deleted login.js in root directory, modified login.js in js directory

--- project/js/login.js
+++ project/js/login.js
@@ -1,3 +1,37 @@
-function getFields(){
-	console.log(document.forms[0])
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+  } else {
+    // No user is signed in.
+  }
+});
+
+
+
+function login(){
+    
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+
+	firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  window.alert("Invalid email and/or password");
+});
+
+}
+
+function register(){
+	
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+	
+	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+
 }
