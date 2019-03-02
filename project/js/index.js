32bb35d0ee86eb3677f391c12a38ec38420d0fad
ArtixBot <williamwongcastle@gmail.com>
2019-03-02 14:58:01 -0500

    Message rendering. Still need to implement retrieval from Firebase.

--- project/js/index.js
+++ project/js/index.js
@@ -1,16 +1,6 @@
 let name = document.getElementById("userID");
 name.innerText = "User";
 
-function requestJSON(url){
-	return fetch(url).then(response => response.json());	// Combined arrow and anonymous function with then().
-}
-
-function submitMsg(){
-	let entry = document.getElementById("chatMessage");
-	console.log(entry.value);
-	entry.value = "";
-}
-
 function getTimeDate(){
 	var datetime = new Date();
 	let time = document.getElementById("systemTime");
@@ -20,9 +10,40 @@ function getTimeDate(){
 	date.innerHTML = datetime.toLocaleDateString();
 }
 
+function requestJSON(url){
+	return fetch(url).then(response => response.json());	// Combined arrow and anonymous function with then().
+}
+
+function submitMsg(){
+	let entry = document.getElementById("submitMessage");
+	if (entry.value == ""){
+		return false;
+	}
+	else{	// Submit to Firebase.
+		renderMessage(entry.value);
+	}
+	entry.value = "";
+}
+
+function renderMessage(message){
+	let chatPanel = document.getElementById("chatPanel");
+	let module = document.createElement("div");
+	module.setAttribute("class", "chatMessage");
+
+	module.innerHTML = message;
+
+	chatPanel.appendChild(module);
+	chatPanel.scrollTop = chatPanel.scrollHeight;
+}
+
+// TODO: Implement. Pull NEW messages from Firebase, then render using renderMessage();
+function retrieveMessages(){
+}
+
 function main(){
 	getTimeDate();
-	setInterval(getTimeDate, 1000);		// Every second update time.
+	setInterval(getTimeDate, 1000);			// Every second update time.
+	setInterval(retrieveMessages, 1000);
 }
 
 main();
