de5b4ce4690c365a3e27bbafb700f357c4cd9ca6
Christopher Raley <cr2fv@virginia.edu>
2019-03-03 10:39:00 -0500

    Password error message

--- project/public/js/login.js
+++ project/public/js/login.js
@@ -13,6 +13,12 @@ login_button.addEventListener('click', e => {
 register_button.addEventListener('click', e => {
 	const email = document.getElementById('email_field').value;
 	const pass = document.getElementById('password_field').value;
+
+    if (pass.length < 6){
+        window.alert("Passwords need to be 6 characters or longer");
+        return;
+    }
+
 	const auth = firebase.auth();
 	//
 	const promise = auth.createUserWithEmailAndPassword(email, pass);
